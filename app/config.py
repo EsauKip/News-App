512995aea5d87a5dc752ebc0c4b392ec5976703b
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-08 23:52:06 +0300

    create source category

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL=' https://newsapi.org/v2/everything?q=apple&from=2022-03-07&to=2022-03-07&sortBy=popularity&apiKey={}'
-
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
 
 
 class ProdConfig(Config):
--- request.py
+++ request.py
@@ -1,4 +1,29 @@
 from app import app
-
+import urllib.request,json
+from .models import article
+Article = article.Article
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
+base_article_url = app.config["ARTICLES_API_BASE_URL"]
+
+def get_sources(source):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
