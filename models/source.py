7dae368330165b7acf72b8d995197c66d2620cc6
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-08 23:57:11 +0300

    add sources class

--- models/source.py
+++ models/source.py
@@ -12,3 +12,16 @@ class Article:
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
         self.content = content
+class Source:
+    '''
+    source class to define source Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+       
+        self.country = country
